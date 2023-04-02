9d8b215e2af0bbe012c49b6a4a8827221449a547
Tero Rantanen <t1rate01@students.oamk.fi>
2023-04-03 00:19:12 +0300

    Userkokeilu pois
    userihommat pois vielä tässä vaiheessa

--- example/src/main/java/com/db/example/visual5/userRepository.java
+++
@@ -1,8 +0,0 @@
-package com.db.example.visual5;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface userRepository extends JpaRepository<user, Long> {
-    
-}
