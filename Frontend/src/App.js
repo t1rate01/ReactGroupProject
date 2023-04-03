af0ec17a3ee2bdf73263006c6fad97d57aa77d23
Tero Rantanen <t1rate01@students.oamk.fi>
2023-04-03 21:39:27 +0300

    Front fixattu

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -2,10 +2,10 @@ import {Bar, Line, Pie} from 'react-chartjs-2';
 import {Chart as ChartJS} from 'chart.js/auto';
 import './App.css';
 import {useState, useEffect} from 'react';
-import SectorChart from './sectorChart';
-import DemoData from './demoPiechart';
-import SubSectorChart from './subSectorChart';
-import BrokenSectorChart from './brokenSectorChart';
+import SectorChart from './Visual5/sectorChart.js';
+import DemoData from './Visual5/demoPiechart';
+import SubSectorChart from './Visual5/subSectorChart';
+import BrokenSectorChart from './Visual5/brokenSectorChart';
 
 
 
