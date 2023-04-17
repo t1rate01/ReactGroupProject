04318dc24242951e53e8cf78051498bda753b012
Cerveku <102629344+Cerveku@users.noreply.github.com>
2023-04-17 18:35:19 +0300

    uppia

--- R3-Projekti-art/example/src/main/java/com/db/example/visual4/visual4Repository.java
+++ R3-Projekti-art/example/src/main/java/com/db/example/visual4/visual4Repository.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface visual4Repository extends JpaRepository<visual4, Long> {
+public interface visual4Repository extends JpaRepository<Visual4Data, Long> {
 
     
 }
