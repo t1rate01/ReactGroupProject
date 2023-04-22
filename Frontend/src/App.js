bd9fd07d2e1d88b34ef2acd028db7711f9c5e802
Tero Rantanen <t1rate01@students.oamk.fi>
2023-04-22 18:55:19 +0300

    Init

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,14 +1,24 @@
 
-import MainMenu from './controller/mainmenu.js';
+import Visual2 from './Visual2/visual2Chart';
+import sectorChart from './Visual5/sectorChart'
+import MainMenu from './controller/mainmenu';
+import DemoData from './Visual5/demoPiechart';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 
 
 function App() {   // kutsutaan aluksi vain MainMenu, joka ohjaa logiikkaa
   return (
+    <BrowserRouter>
+    <div>
+      <h1>Projektiryhmä 3</h1>
+      <Routes>
+        <Route path="/" element={<MainMenu />} />
+        <Route path="/demo" element={<DemoData />} />
+        </Routes>
+    </div>
 
-
-        <MainMenu />
-
+    </BrowserRouter>
   );
 }
 
