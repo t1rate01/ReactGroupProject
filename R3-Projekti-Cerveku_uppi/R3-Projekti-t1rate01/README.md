03f4fcb0508e33609cbc8ea6cf09b72ee83dfc82
Art Karimäki <art.karimaki@gmail.com>
2023-04-25 23:19:41 +0300

    Delete R3-Projekti-Cerveku_uppi/R3-Projekti-t1rate01 directory

--- R3-Projekti-Cerveku_uppi/R3-Projekti-t1rate01/README.md
+++
@@ -1,6 +0,0 @@
-# R3-Projekti
-
-t1rate01 = Tero Rantanen  
-saattaja = Janita Kaikkonen  
-akulonpa = Alisa Kulonpää  
-cerveku = Art Karimäki  
--- R3-Projekti-Cerveku_uppi/R3-Projekti-t1rate01/example/src/main/java/com/db/example/visual5/brokensectorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.db.example.visual5;
-
-import org.springframework.stereotype.Repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-@Repository
-public interface brokensectorRepository extends JpaRepository<subsector_breakdown, Long>{
-    
-}
