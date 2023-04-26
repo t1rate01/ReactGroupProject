d2ed1115f80ddbf67be426e8b4d2b3f4dcd39318
saattaja <janita.elisa@outlook.com>
2023-04-26 19:55:55 +0300

    scrollbutton mutta ei pelaa

--- Frontend/src/frontpage_login_register/showall.js
+++ Frontend/src/frontpage_login_register/showall.js
@@ -6,11 +6,13 @@ import Visual2 from "../Visual2/visual2Chart";
 import MyVisual3Chart from "../Visual3/visual3Chart";
 import Visual4Chart from "../Visual4/visual4Chart";
 import SectorChart from "../Visual5/sectorChart";
+import ScrollButton from "./scrollButton.js";
 
 const ShowAll = () => {
 
     return (
         <div className="frontpage"> 
+        
         <div className="frontpage"><h1>Here is all visuals listed</h1>
         <p>In this page you can see all of the charts. More info and source links are 
             visible by clicking the "Info" button below every chart.</p></div>
@@ -20,8 +22,9 @@ const ShowAll = () => {
             <div><Visual4Chart/></div>
             <div><SectorChart /></div>
             <Link to="/"><button id="backtofront">Back</button></Link>
+            
         </div>
-    )
+    );
 
 }
 
