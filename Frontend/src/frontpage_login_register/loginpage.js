67e043bcf3d44e743f7359422fed21f0b958e0b2
Tero Rantanen <t1rate01@students.oamk.fi>
2023-05-01 20:47:26 +0300

    Frontin testit 75%

--- Frontend/src/frontpage_login_register/loginpage.js
+++ Frontend/src/frontpage_login_register/loginpage.js
@@ -9,14 +9,24 @@ const LoginPage = () => {
     const navigate = useNavigate();
     const defaultViewCompareString = "0,0,0,0,0,0"; // ohjelma tallentaa näin
     const defaultViewCompareString2 = "000000" // databasen default toiminto tallentaa näin
+    const [loginFail, setLoginFail] = useState(false);
+    const [loginSuccess, setLoginSuccess] = useState(false);
 
     const loginBtn = async (event) => {
        event.preventDefault();   // event objektista löytyy, estää selaimen sekaantumisen formiin(?) ja sen sijaan mennään omilla toiminnoilla
        if(username === "" || password === "") {
-              alert("Username or password missing");
+            handleLoginFail();
               return;
          }
        await login(username, password);
+    }
+
+    const handleLoginFail = () => {
+        setLoginFail(true);
+    }
+
+    const handleLoginSuccess = () => {
+        setLoginSuccess(true);
     }
 
     async function login(username, password) {
@@ -31,12 +41,14 @@ const LoginPage = () => {
         const data = await response.text();
         console.log(response.status)
         setToken(data);
+        handleLoginSuccess();
+        await new Promise(resolve => setTimeout(resolve, 1000)); // annetaan teksti näkyy hetken
         //console.log("Token datassa on " +data);
         //console.log("Token on "+ getToken());
        await checkDefaultView();
     }   
     else {
-        alert("Wrong username or password");
+        handleLoginFail();
         console.log(response.status)
     }
     }
@@ -67,17 +79,21 @@ const LoginPage = () => {
 
     return (
         <div className="frontpage">
-            <h1>Log in</h1>
+            {loginSuccess ? (<h1>Success!</h1>) : ( <>
+            {loginFail ? (
+                <h1>Login failed!</h1>
+                 ) : (
+                    <h1>Login</h1>)}</>) }
             <form onSubmit={loginBtn}>
                 <div id="checkboxes"><label>
                     Username:
-                    <input type="text" name="username" value={username} onChange={(event)=> setUsername(event.target.value)}/>
+                    <input type="text" data-testid="username" name="username" value={username} onChange={(event)=> setUsername(event.target.value)}/>
                 </label></div>
                 <div id="checkboxes"><label>
                     Password:
-                    <input type="password" name="password" value={password} onChange={(event)=> setPassword(event.target.value)} />
+                    <input type="password" data-testid="password" name="password" value={password} onChange={(event)=> setPassword(event.target.value)} />
                 </label></div>
-                <button id="buttons" type="submit">Login</button>
+                <button id="buttons" data-testid="logbtn" type="submit">Login</button>
             </form>
             <Link to="/"><button id="buttons">Back</button></Link>
         </div>
