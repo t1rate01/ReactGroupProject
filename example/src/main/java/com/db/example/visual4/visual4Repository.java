2257a6aecade8b1ef770738526f40a7da9c23f5d
Cerveku <102629344+Cerveku@users.noreply.github.com>
2023-05-02 19:11:42 +0300

    kommentit lisatty

--- example/src/main/java/com/db/example/visual4/visual4Repository.java
+++ example/src/main/java/com/db/example/visual4/visual4Repository.java
@@ -1,9 +1,9 @@
 package com.db.example.visual4;
 
-import java.util.List;
+import java.util.List;  // import the List class
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;  // import the JpaRepository interface
+import org.springframework.stereotype.Repository;  // import the Repository annotation
 
 @Repository
 public interface visual4Repository extends JpaRepository<visual4Data, Long> {
