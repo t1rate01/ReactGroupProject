6a43624ab8a31465e7563827e22607e2063d4c44
Tero Rantanen <t1rate01@students.oamk.fi>
2023-05-02 21:43:02 +0300

    Fix

--- example/src/main/java/com/db/example/visual4/visual4Repository.java
+++ example/src/main/java/com/db/example/visual4/visual4Repository.java
@@ -1,6 +1,10 @@
 package com.db.example.visual4;
 
+<<<<<<< Updated upstream
 import java.util.List;
+=======
+
+>>>>>>> Stashed changes
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
